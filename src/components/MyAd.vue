b777061b389836e7932ba4e50a10db7d10369b21
1607562910 <1607562910@qq.com>
2022-11-11 15:21:50 +0800

    x

---
+++ src/components/MyAd.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="my-ad">
+    <a href="https://github.com/yepi-bit" target="_blank">
+      <div style="background: rgba(0, 0, 0, 0.8); padding: 12px">
+        <github-outlined />
+        代码完全开源，欢迎 star
+      </div>
+    </a>
+  </div>
+</template>
+<script setup lang="ts">
+import { GithubOutlined } from "@ant-design/icons-vue";
+</script>
+<style></style>
